852d96b54b296d3ccfad5dc90aed137d5f12efed
Ji Wook Jeong <doksakim77@naver.com>
2022-04-22 18:16:53 +0900

    build: 초기 프로젝트 설정

---
+++ backend/src/test/java/com/ssafy/barguni/BarguniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.barguni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarguniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
