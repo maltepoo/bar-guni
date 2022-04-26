43ab7d2a36a71f01d5bd4b93da675a2edfeeb91b
Ji Wook Jeong <doksakim77@naver.com>
2022-04-26 19:43:54 +0900

    refactor: 바구니 상세 조회 리팩토링 (#S06P31B202-106)

--- backend/src/main/java/com/ssafy/barguni/api/basket/repository/CustomBasketRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.barguni.api.basket.repository;
-
-import com.ssafy.barguni.api.basket.entity.Basket;
-
-public interface CustomBasketRepository {
-    Basket getByIdWithPic(Long id);
-}
