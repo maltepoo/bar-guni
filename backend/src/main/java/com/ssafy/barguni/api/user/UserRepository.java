ae4b09aa9372cdb7b752bb0cc6a461c12cf47535
DooHee <dh73111@gmail.com>
2022-04-26 14:13:59 +0900

    feat : 유저 CRUD rest api 틀 생성 (#S06P31B202-79)

--- backend/src/main/java/com/ssafy/barguni/api/user/UserRepository.java
+++ backend/src/main/java/com/ssafy/barguni/api/user/UserRepository.java
@@ -1,6 +1,8 @@
 package com.ssafy.barguni.api.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +11,10 @@ public interface UserRepository extends JpaRepository<User, Long>, CustomUserRep
     Optional<User> findByEmail(String email);
     Optional<User> findById(Long id);
     Boolean existsByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE User u SET u.name = :name WHERE u.id = :id")
+    User modifyUser(Long id, String name);
+
+    void deleteById(Long id);
 }
