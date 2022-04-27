c47184bf3c5ecdf8f6fa18426e377e99620202b1
Ji Wook Jeong <doksakim77@naver.com>
2022-04-27 14:20:57 +0900

    feat: 바코드로 제품 정보 조회 기능 개발 (#S06P31B202-98)

--- backend/src/main/java/com/ssafy/barguni/api/product/ProductService.java
+++ backend/src/main/java/com/ssafy/barguni/api/product/ProductService.java
@@ -1,10 +1,19 @@
 package com.ssafy.barguni.api.product;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.barguni.api.Picture.Picture;
+import com.ssafy.barguni.api.product.vo.BarcodeDataRes;
 import com.ssafy.barguni.common.util.NaverImgSearchUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @RequiredArgsConstructor
@@ -17,12 +26,23 @@ public class ProductService {
         if (prodRepository.existsProductByBarcode(barcode)) {
             return prodRepository.findByBarcode(barcode).get();
         } else {
+            BarcodeDataRes barcodeData = null;
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                // 바코드로 정보 조회
+                barcodeData = objectMapper.readValue(getData(barcode).getBody(), BarcodeDataRes.class);
+                // 바코드 조회 실패
+                if(barcodeData.getBarcodeData().getTotalCount() == 0) return null;
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            String name = barcodeData.getBarcodeData().getRow().get(0).getPrdlstNm();
+
             //바코드로 사진 이름찾기, 사진 Picture 객체로 만들어 저장하기.
-            String name = "안녕";
             Picture pic = new Picture();
 
             Product newProd = Product.createProduct(pic, barcode, name);
-
             return prodRepository.save(newProd);
         }
 
@@ -32,4 +52,28 @@ public class ProductService {
         return NaverImgSearchUtil.imageSearch(word);
     }
 
+    private ResponseEntity<String> getData(String barcode) {
+        ResponseEntity<String> tokens = null;
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+//        params.add("BAR_CD", barcode);
+
+        HttpEntity<MultiValueMap<String, String>> barcodeDataRequest =
+                new HttpEntity<>(params, headers);
+        try {
+            tokens = rt.exchange(
+                    "http://openapi.foodsafetykorea.go.kr/api/8098e732d9f3451e9955/C005/json/1/1/BAR_CD="+barcode,
+                    HttpMethod.GET,
+                    barcodeDataRequest,
+                    String.class
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return tokens;
+    }
 }
