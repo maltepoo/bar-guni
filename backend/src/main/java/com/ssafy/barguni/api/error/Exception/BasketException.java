d3e25f9c16f548e796a0211e46f4e48e22026a87
Ji Wook Jeong <doksakim77@naver.com>
2022-04-27 22:02:15 +0900

    refactor: 커스텀 예외 클래스 정의 및 상속 관계 형성 (#S06P31B202-100)

--- backend/src/main/java/com/ssafy/barguni/api/error/Exception/BasketException.java
+++ backend/src/main/java/com/ssafy/barguni/api/error/Exception/BasketException.java
@@ -2,15 +2,16 @@ package com.ssafy.barguni.api.error.Exception;
 
 import com.ssafy.barguni.api.error.ErrorCode;
 import com.ssafy.barguni.api.error.ErrorResVO;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class BasketException extends RuntimeException{
+public class BasketException extends CustomException{
     private static final long serialVersionUID = 1L;
-    private ErrorResVO errorResVO;
+
+    public BasketException(ErrorResVO errorResVO){
+        super(errorResVO);
+    }
 
 }
--- backend/src/main/java/com/ssafy/barguni/api/error/Exception/JwtException.java
+++ backend/src/main/java/com/ssafy/barguni/api/error/Exception/JwtException.java
@@ -1,14 +1,15 @@
 package com.ssafy.barguni.api.error.Exception;
 
 import com.ssafy.barguni.api.error.ErrorResVO;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class JwtException extends RuntimeException{
+public class JwtException extends CustomException{
     private static final long serialVersionUID = 1L;
-    private ErrorResVO errorResVO;
+
+    public JwtException(ErrorResVO errorResVO){
+        super(errorResVO);
+    }
 }
