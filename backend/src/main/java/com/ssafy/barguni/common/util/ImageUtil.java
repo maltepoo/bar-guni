4159a71ca8c527d594b75203c166b88db0484691
junaem <junh9547@gmail.com>
2022-04-28 01:48:15 +0900

    feat : 제품명으로 검색하여 이미지 저장 (#S06P31B202-124)

--- backend/src/main/java/com/ssafy/barguni/common/util/ImageUtil.java
+++ backend/src/main/java/com/ssafy/barguni/common/util/ImageUtil.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
@@ -24,7 +26,7 @@ public class ImageUtil {
         uploadFolder = path;
     }
 
-    public static Picture create(MultipartFile multipartFile, String entity){
+    public static Picture create(MultipartFile multipartFile, String entity) {
         Picture picture = new Picture();
         String imageFileName = entity + "_" + getRandomCode() + "_" + Long.toString(System.currentTimeMillis()) + "." + FilenameUtils.getExtension(multipartFile.getOriginalFilename());
         Path imageFilePath = Paths.get(uploadFolder + imageFileName);
@@ -38,10 +40,36 @@ public class ImageUtil {
             picture.setTitle(multipartFile.getOriginalFilename());
             picture.setPicUrl(imageFileName);
             picture.setCreatedAt(LocalDateTime.now());
+        } else {
+            System.out.println("getsize 실패");   //이거는 있어도 될 것 같아서 남겨둡니다
         }
 
         return picture;
     }
+
+// MultipartFile로 변환하지 않고 bufferedImage인 상태로 저장하는 메서드인데 나중에
+// 실행속도 개선할 때 쓸 수도 있을것 같아서 놔둡니다
+//    public static Picture createByBufferedImg(BufferedImage bufferedImage, String entity, String name, String imgExtension){
+//        Picture picture = new Picture();
+//        String imageFileName = entity + "_" + getRandomCode() + "_" + Long.toString(System.currentTimeMillis()) + "." + imgExtension;
+//        Path imageFilePath = Paths.get(uploadFolder + imageFileName);
+//
+//        if (bufferedImage.getData() != null) { // 파일이 업로드 되었는지 확인
+//            try {
+//                File file = imageFilePath.toFile();
+//                ImageIO.write(bufferedImage, imgExtension, file);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//            picture.setTitle(name);
+//            picture.setPicUrl(imageFileName);
+//            picture.setCreatedAt(LocalDateTime.now());
+//        }else{
+//            System.out.println("getData 실패");
+//        }
+//
+//        return picture;
+//    }
 
     public static Boolean delete(Picture picture){
         String entity = picture.getPicUrl().substring(0,picture.getPicUrl().lastIndexOf("_"));
