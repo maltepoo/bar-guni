a8f18225e32928ac818d75d2dce1435dc19efd62
mp <mpcbpro@gmail.com>
2022-04-29 16:55:43 +0900

    Feat : 로그인 화면 레이아웃 (#S06P31B202-99)

--- BarguniApp/src/pages/Login.tsx
+++ BarguniApp/src/pages/Login.tsx
@@ -1,8 +1,50 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image, StyleSheet, TextInput, Button} from 'react-native';
+// @ts-ignore
+import logo from '../assets/loginlogo.png';
 
 function Login() {
-  return <Text>Login Screen</Text>;
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logo}
+        resizeMode={'contain'}
+        style={{width: 200, height: 200, marginBottom: 20}}
+      />
+      <TextInput style={styles.textInput} placeholder="baguni@baguni.com" />
+      <TextInput style={styles.textInput} placeholder="********" />
+      <Text>비밀번호를 잊어버리셨나요?</Text>
+      <View style={{marginTop: 10, marginBottom: 10}}>
+        <Text>회원가입</Text>
+      </View>
+      <View style={styles.button}>
+        <Button title="로그인"></Button>
+        <Button title="카카오 로그인" color="#FEE500"></Button>
+      </View>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+  },
+  textInput: {
+    padding: 5,
+    marginTop: 10,
+    height: 30,
+    margin: 12,
+    borderWidth: 1,
+    width: '60%',
+    textAlign: 'center',
+    borderRadius: 10,
+  },
+  button: {
+    borderRadius: 10,
+    width: '60%',
+  },
+});
 
 export default Login;
