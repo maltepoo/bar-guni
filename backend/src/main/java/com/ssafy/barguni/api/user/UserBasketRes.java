f8a1957f1604e16cc77259d025db3c4f7fcb176c
Ji Wook Jeong <doksakim77@naver.com>
2022-05-05 13:35:13 +0900

    fix: 유저 참여 바구니 조회 시, response 데이터에 바구니 이름 추가 (#S06P31B202-142)

--- backend/src/main/java/com/ssafy/barguni/api/user/UserBasketRes.java
+++ backend/src/main/java/com/ssafy/barguni/api/user/UserBasketRes.java
@@ -13,17 +13,20 @@ public class UserBasketRes {
     private Long user_id;
     private Long bkt_id;
     private UserAuthority authority;
+    private String bkt_name;
 
-    public UserBasketRes(Long id, Long user_id, Long bkt_id, UserAuthority authority) {
+    public UserBasketRes(Long id, Long user_id, Long bkt_id, UserAuthority authority, String bkt_name) {
         this.id = id;
         this.user_id = user_id;
         this.bkt_id = bkt_id;
         this.authority = authority;
+        this.bkt_name = bkt_name;
     }
 
 
     private static UserBasketRes convertToUbRes(UserBasket ub) {
-        return new UserBasketRes(ub.getId(), ub.getUser().getId(), ub.getBasket().getId(), ub.getAuthority());
+        return new UserBasketRes(ub.getId(), ub.getUser().getId()
+                , ub.getBasket().getId(), ub.getAuthority(), ub.getBasket().getName());
     }
 
     public static List<UserBasketRes> convertToUbResList(List<UserBasket> ubList) {
