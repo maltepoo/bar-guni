ac60d58abb55f7979120776b38f0c3a795b222cf
maltepoo <jaengpin@gmail.com>
2022-05-06 23:29:10 +0900

    feat: 회원이름변경 api 연결

--- BarguniApp/src/pages/MyPage.tsx
+++ BarguniApp/src/pages/MyPage.tsx
@@ -1,8 +1,56 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {
+  Alert,
+  ScrollView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+} from 'react-native';
+import {changeName, getProfile} from '../api/user';
 
 function MyPage() {
-  return <Text>회원정보페이지</Text>;
+  const [userInfo, setUserInfo] = useState({});
+  const [newName, setNewName] = useState('');
+
+  useEffect(() => {
+    init();
+  }, []);
+  const init = useCallback(async () => {
+    const userInfo = await getProfile();
+    console.log(userInfo, '유저인포');
+    setUserInfo(userInfo);
+  }, []);
+
+  const handleChangeName = useCallback(name => {
+    console.log(name);
+    setNewName(name);
+  });
+
+  const modifyUserName = useCallback(async () => {
+    try {
+      const res = await changeName(newName);
+      console.log(res, '결과');
+      await Alert.alert('변경완료되었습니다.');
+    } catch (err) {
+      console.log(err, 'userName change ERROR');
+    }
+  });
+
+  return (
+    <ScrollView>
+      <Text>회원정보</Text>
+      <Text>{userInfo.email}</Text>
+      <Text>{userInfo.name}</Text>
+      <TextInput
+        placeholder={'회원이름입력폼'}
+        value={newName}
+        onChangeText={handleChangeName}
+      />
+      <TouchableOpacity onPress={modifyUserName}>
+        <Text>회원이름수정</Text>
+      </TouchableOpacity>
+    </ScrollView>
+  );
 }
 
 export default MyPage;
