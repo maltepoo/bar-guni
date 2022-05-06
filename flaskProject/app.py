6845d0f3e11d7980412b5a43235864993808966d
Ji Wook Jeong <doksakim77@naver.com>
2022-05-06 15:30:13 +0900

    feat: 영수증 분석에 필터 적용, multipart 통신 적용, 공공데이터 통신 코드 분리 (#S06P31B202-145)

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,59 +1,51 @@
-import requests
-from flask import Flask
+from flask import Flask, request
 import pytesseract
 from PIL import Image
 import requests
+from Filter import TrieNode
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './receipts'
+
+filter_words = ["커피", "우유", "비빔면", "햄버거", "라면", "상추", "술", "포도", "돈까스", "김밥", "어니언", "초코", "사과"]
+root = TrieNode()
+for word in filter_words:
+    root.add(word,0)
 
 
-@app.route('/')
+@app.route('/',methods=['POST'])
 def barcode():  # put application's code here
-    result1 = pytesseract.image_to_string(Image.open('./static/img_2.png'), lang='kor')
-    print(result1)
-    words = result1.split()
-    api_key = "%2FFcYlFOefSczcSw8Ra5SXrup1ENH5ohszaxb%2FA8dF5l9NZIFW3f4faSbqHKRJmxPmU9505mjU1OwIp5mw9EoYQ%3D%3D"
-    api_key2 = "/FcYlFOefSczcSw8Ra5SXrup1ENH5ohszaxb/A8dF5l9NZIFW3f4faSbqHKRJmxPmU9505mjU1OwIp5mw9EoYQ=="
+    # 파라미터로 multipart 파일이 없을 경우
+    if request.method == 'POST':
+        if 'receipt' not in request.files:
+            return 'File is missing', 404
+
+    receipt = request.files['receipt']
+    # ocr 처리
+    result = pytesseract.image_to_string(Image.open(receipt), lang='kor')
+    # 단어 빈칸으로 구분
+    words = result.split()
 
     result_li = []
-
-    # words = ["피코크 초마짬뽕 12", "(G)피코크 레이디핑", "무지개 방울토마토9", "델몬트파머스주스2", "서울 저지방우유 1L"]
-
     for word in words:
         # word = ''.join(filter(str.isalnum, word)) # 숫자만
         word = ''.join(filter(str.isalpha, word))   # 언어만
 
-        #한글만
-        # n_word= ''
-        # for c in word:
-        #     if '가' <= c <= '힣':
-        #         n_word+=c
-        # word = n_word
+        # 필터 처리
+        st_idx = 0
+        check = False
+        while st_idx < len(word) and check == False:
+            check = root.search(word, st_idx)
+            st_idx += 1
 
-        if len(word)<2: continue
+        if(check == False):
+            continue
 
-        url = "http://apis.data.go.kr/B553748/CertImgListService/getCertImgListService"
-        url += "?ServiceKey="+api_key + "&prdlstNm="+word + "&returnType=json"
+        # 필터 통과하면 결과로 저장
+        result_li.append(word)
 
-        res = requests.get(url)
-        if res.status_code != 200: continue
-        js = res.json()
-        print(word)
-        if js['totalCount'] != '0':
-            # result_li.append(word)
-            result_li.append(js)
-
-
-   # print(pytesseract.image_to_string(Image.open('./static/img_1.png'), lang='kor_vert'))
-    # result1 = pytesseract.image_to_string(Image.open('./static/ljh_best.png'), lang='kor')
-
-    # return result1
     return str(result_li)
-@app.route('/reciept')
-def rec():  # put application's code here
 
-
-    return 'receipt'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
