7151f140fcd38768d868a9a177a01d9464dde1bb
박다원 <dawon950@gmail.com>
2022-05-09 09:49:10 +0900

    Feat : 휴지통화면 헤더 (#S06P31B202-193)

--- BarguniApp/src/pages/TrashCan.tsx
+++ BarguniApp/src/pages/TrashCan.tsx
@@ -1,25 +1,58 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
 import TrashItem from '../components/TrashItem';
+import {Checkbox} from 'react-native-paper';
+import {Button} from '@rneui/base';
 
 function TrashCan() {
   const [items, setItems] = useState([{id: 11}, {id: 22}]);
+  const [checked, setChecked] = useState(false);
   const renderItem = useCallback(({item}: {item: object}) => {
     return <TrashItem item={item}></TrashItem>;
   }, []);
   return (
-    <View>
-      <View style={Style.container}>
-        <Text style={Style.title}>휴지통 페이지</Text>
+    <View style={Style.container}>
+      <View style={Style.header}>
+        <Checkbox
+          color="#0094FF"
+          status={checked ? 'checked' : 'unchecked'}
+          onPress={() => {
+            setChecked(!checked);
+          }}
+        />
+        <Text style={Style.headerText}>전체 선택</Text>
+        <Button
+          title={'복원'}
+          titleStyle={Style.buttonText}
+          buttonStyle={Style.restore}
+        />
+        <Button
+          title={'삭제'}
+          titleStyle={Style.buttonText}
+          buttonStyle={Style.remove}
+        />
       </View>
-      <View style={Style.line}></View>
       <FlatList data={items} renderItem={renderItem} />
     </View>
   );
 }
 const Style = StyleSheet.create({
   container: {
-    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  remove: {
+    backgroundColor: 'red',
+  },
+  buttonText: {
+    fontSize: 10,
+  },
+  restore: {marginRight: 5},
+  headerText: {
+    color: 'black',
+    marginTop: 7,
+    fontWeight: 'bold',
+    width: '65%',
+    marginLeft: 10,
   },
   title: {
     fontSize: 30,
@@ -27,6 +60,7 @@ const Style = StyleSheet.create({
     color: 'black',
     marginTop: 10,
   },
+  header: {flexDirection: 'row', marginTop: 10},
   line: {width: '100%', height: 0.7, backgroundColor: 'gray'},
 });
 export default TrashCan;
