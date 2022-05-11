a0295d7e8fc00031ad78b485e7eda50b29768611
Ji Wook Jeong <doksakim77@naver.com>
2022-05-11 19:29:20 +0900

    feat: 아이템 삭제 기능 - alert -> item -> picture 순서로 삭제  (#S06P31B202-186)

--- backend/src/main/java/com/ssafy/barguni/api/item/ItemRepository.java
+++ backend/src/main/java/com/ssafy/barguni/api/item/ItemRepository.java
@@ -1,6 +1,8 @@
 package com.ssafy.barguni.api.item;
 
+import com.ssafy.barguni.api.basket.entity.Basket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -8,6 +10,8 @@ import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Long>, CustomItemRepository {
     Optional<Item> findById(Long id);
+    @Query("select i from Item i left join fetch i.picture p where i.id = :id")
+    Item findWithPictureById(Long id);
     List<Item> findAllByBasketId(Long bktId);
     Integer countByBasketId(Long basketId);
     @Query("select i from Item i join fetch i.basket b")
@@ -15,4 +19,5 @@ public interface ItemRepository extends JpaRepository<Item, Long>, CustomItemRep
     Integer deleteItemsByBasket_IdAndUsed(Long bktId, Boolean used);
     @Query("select i from Item i left join fetch i.category left join fetch i.picture where i.basket.id = :bktId And i.used = :used")
     List<Item> findItemsByBasket_IdAndUsed(Long bktId, Boolean used);
+    Integer deleteItemsByBasket(Basket basket);
 }
