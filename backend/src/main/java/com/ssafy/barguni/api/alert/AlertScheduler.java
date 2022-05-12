49385fefbe02af9d9a35655aa00d42f6de556a81
Ji Wook Jeong <doksakim77@naver.com>
2022-05-12 17:28:30 +0900

    fix: 알람 스케쥴러 오류 처리 (#S06P31B202-138)

--- backend/src/main/java/com/ssafy/barguni/api/alert/AlertScheduler.java
+++ backend/src/main/java/com/ssafy/barguni/api/alert/AlertScheduler.java
@@ -4,6 +4,7 @@ import com.ssafy.barguni.api.item.AlertBy;
 import com.ssafy.barguni.api.item.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -23,14 +24,25 @@ public class AlertScheduler {
     private final ItemService itemService;
 
     // (초 분 시 일 월)
-    @Scheduled(cron="0 0 1 * * ?") // 매일 오전 1시에 동작
+//    @Scheduled(cron="0 0 1 * * ?") // 매일 오전 1시에 동작
+    @Scheduled(cron="${schedular.alert.time}")
     @Async
     public void createAlert(){
         long start = System.currentTimeMillis();
-
+/*
         itemService.findAll().forEach((item)->{
             // 이미 사용한 경우는 제외
             if(item.getUsed())
+                return;
+
+            // 기간 설정이 잘 못된 경우 알림 생성 불가능
+            if(item.getAlertBy() == null)
+                return;
+            else if(item.getAlertBy() == AlertBy.D_DAY
+                    && (item.getRegDate() == null || item.getDDAY() == null))
+                return;
+            else if(item.getAlertBy() == AlertBy.SHELF_LIFE
+                    && item.getShelfLife() == null)
                 return;
 
             // 유통기한 지남 알림
@@ -56,7 +68,7 @@ public class AlertScheduler {
             ) alertService.createAlertBeforeExpiry(item);
 
         });
-
+*/
         long end = System.currentTimeMillis();
         log.debug("총 걸린 시간: " + (end - start) + " ms");
     }
